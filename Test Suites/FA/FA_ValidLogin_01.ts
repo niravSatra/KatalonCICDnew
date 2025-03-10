ef2535597bbd051c30fcfaeaf8e43e12442dc214
Nirav.Satra <Nirav.Satra@AQMIT-LPT-11113>
2025-03-10 20:32:23 +0530

    c3

---
+++ Test Suites/FA/FA_ValidLogin_01.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>FA valid login</description>
+   <name>FA_ValidLogin_01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2ffa40df-7943-46ad-b6e4-04538b3a076a</testSuiteGuid>
+   <testCaseLink>
+      <guid>6ab9b57c-1433-4a5b-82c1-a48102fdceef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/FA_ValidLogin_01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
