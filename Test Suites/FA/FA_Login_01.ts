d5b6474e4ca1ca875285fd5f07de7e830b293cd0
Nirav.Satra <Nirav.Satra@AQMIT-LPT-11113>
2025-03-10 15:26:00 +0530

    test suite

---
+++ Test Suites/FA/FA_Login_01.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>FA_Login_01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>55ce3827-526a-4822-9b94-9e2dc48d07d0</testSuiteGuid>
+   <testCaseLink>
+      <guid>279ef30c-7467-4355-be15-bf56701c9e8a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/FA_Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
